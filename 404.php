f15ea8b3d4f45e2d4e5a8df7d859f8815486a9d2
Christopher Coleman <freshyill@gmail.com>
2013-11-19 11:12:27 -0500

    404 page, images

--- 404.php
+++ 404.php
@@ -16,15 +16,16 @@
     
       <div class="notfoundcontent">
       
+        <h1>Nothing Found</h1>
        
-       <div class="searchblock">
-       <p><b>Not Found</b>. He sees you when you're sleeping, but Santa couldn't find the page you were looking for.</p>
-       <?php get_template_part('searchform'); ?>
-       </div> 
+        <div class="searchblock">
+          <p>He sees you when you're sleeping, but Santa couldn't find the page you were looking for.</p>
+          <?php get_template_part('searchform'); ?>
+        </div> 
         
        <div class="santa-animated">
-         <img src="<?php echo bloginfo(template_directory); ?>/img/404-santa-face.png">
-         <img src="<?php echo bloginfo(template_directory); ?>/img/404-santa-eyes.png">
+        <div class="eyes"><img src="<?php echo bloginfo(template_directory); ?>/img/404-santa-eyes.png" class="eyes"></div>
+        <div class="face"><img src="<?php echo bloginfo(template_directory); ?>/img/404-santa-face.png" class="face"></div>
       </div>
       
       </div>
